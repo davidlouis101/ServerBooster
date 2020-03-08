33a0972e56d5761964495f91164d203a049fe8db
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-03-08 10:48:24 +0100

    Update FlyBooster.php

--- src/ceepkev77/FlyBooster.php
+++ src/ceepkev77/FlyBooster.php
@@ -24,15 +24,15 @@ class FlyBooster extends Task
             $this->plugin->fsek--;
             if($this->plugin->fsek === 30) {
                 foreach ($this->plugin->getServer()->getOnlinePlayers() as $p) {
-                         $p->sendMessage("§bBooster§c §8»§l§aDer FlyBooster wird in §c30 sekunden §adeaktiviert.");
+                         $p->sendMessage("§cBoosterSystem§b §8»§l§aDer FlyBooster wird in §c30 sekunden §adeaktiviert.");
                          }
             }elseif($this->plugin->fsek === 10) {
                 foreach ($this->plugin->getServer()->getOnlinePlayers() as $p) {
-                         $p->sendMessage("§bBooster§c §8»§l§aDer FlyBooster wird in §c10 sekunden §adeaktiviert.");
+                         $p->sendMessage("§cBoosterSystem§b §8»§l§aDer FlyBooster wird in §c10 sekunden §adeaktiviert.");
                          }
             }elseif($this->plugin->fsek === 0) {
                 foreach ($this->plugin->getServer()->getOnlinePlayers() as $p) {
-                         $p->sendMessage("§bBooster§c §8»§l§aDer FlyBooster wurde nun deaktiviert. \n\n§r§aDu kannst nun nicht mehr Fliegen.");
+                         $p->sendMessage("§cBoosterSystem§b §8»§l§aDer FlyBooster wurde nun deaktiviert. \n\n§r§aDu kannst nun nicht mehr Fliegen.");
                     if($p->getGamemode() == 0) {
                         $p->setFlying(FALSE);
                         $p->setAllowFlight(FALSE);
